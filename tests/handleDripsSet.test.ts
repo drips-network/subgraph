edee24a2e394f00d750ca1e920e2c6ebb82441c9
Ioannis Tourkogiorgis <jtourkos@gmail.com>
2023-02-27 14:21:13 +0100

    test: add DripsSet event mapper tests

--- tests/handleDripsSet.test.ts
+++ tests/handleDripsSet.test.ts
@@ -1,15 +1,150 @@
-import { BigInt } from '@graphprotocol/graph-ts';
-import { assert, describe, test } from 'matchstick-as';
+import { BigInt, Bytes, ethereum } from '@graphprotocol/graph-ts';
+import { afterEach, assert, clearStore, describe, test, log, logStore } from 'matchstick-as';
+import {
+  DripsEntry,
+  DripsSetEvent,
+  LastSetDripsUserMapping,
+  User,
+  UserAssetConfig
+} from '../generated/schema';
+import { handleDripsSet } from '../src/mapping';
 import { createDripsSetEvent } from './helpers/eventCreators';
 
 describe('handleDripsSet', () => {
-  test('should create a new user', () => {
+  afterEach(() => {
+    clearStore();
+  });
+
+  test('should create entities as expected when mapping', () => {
     // Arrange
-    const dripsSetEvent = createDripsSetEvent(BigInt.fromI32(1), BigInt.fromI32(1));
+    const incomingDripsSetEvent = createDripsSetEvent(
+      BigInt.fromI32(1),
+      BigInt.fromI32(2),
+      Bytes.fromUTF8('receiversHash'),
+      Bytes.fromUTF8('dripsHistoryHash'),
+      BigInt.fromI32(3),
+      BigInt.fromI32(4),
+      BigInt.fromI32(5),
+      Bytes.fromUTF8('transactionHash')
+    );
 
     // Act
+    handleDripsSet(incomingDripsSetEvent);
 
     // Assert
-    assert.stringEquals(dripsSetEvent.params.userId.toString(), '1');
+    const user = User.load(incomingDripsSetEvent.params.userId.toString());
+    assert.assertNotNull(user);
+
+    const userAssetConfigId = `${incomingDripsSetEvent.params.userId.toString()}-${incomingDripsSetEvent.params.assetId.toString()}`;
+    const userAssetConfig = UserAssetConfig.load(userAssetConfigId) as UserAssetConfig;
+    assert.bigIntEquals(userAssetConfig.balance, incomingDripsSetEvent.params.balance);
+    assert.bytesEquals(userAssetConfig.assetConfigHash, incomingDripsSetEvent.params.receiversHash);
+    assert.bigIntEquals(
+      userAssetConfig.lastUpdatedBlockTimestamp,
+      incomingDripsSetEvent.block.timestamp
+    );
+    assert.arrayEquals(
+      userAssetConfig.dripsEntryIds.map<ethereum.Value>((s) => ethereum.Value.fromString(s)),
+      []
+    );
+
+    const dripsSetEventId = `${incomingDripsSetEvent.transaction.hash.toHexString()}-${incomingDripsSetEvent.logIndex.toString()}`;
+    const dripsSetEventEntity = DripsSetEvent.load(dripsSetEventId) as DripsSetEvent;
+    assert.stringEquals(dripsSetEventEntity.userId, incomingDripsSetEvent.params.userId.toString());
+    assert.bigIntEquals(dripsSetEventEntity.assetId, incomingDripsSetEvent.params.assetId);
+    assert.bytesEquals(
+      dripsSetEventEntity.receiversHash,
+      incomingDripsSetEvent.params.receiversHash
+    );
+    assert.bytesEquals(
+      dripsSetEventEntity.dripsHistoryHash,
+      incomingDripsSetEvent.params.dripsHistoryHash
+    );
+    assert.bigIntEquals(dripsSetEventEntity.balance, incomingDripsSetEvent.params.balance);
+    assert.bigIntEquals(dripsSetEventEntity.maxEnd, incomingDripsSetEvent.params.maxEnd);
+    assert.bigIntEquals(dripsSetEventEntity.blockTimestamp, incomingDripsSetEvent.block.timestamp);
+
+    const lastDripsSetUserMappingId = incomingDripsSetEvent.params.receiversHash.toHexString();
+    const lastDripsSetUserMapping = LastSetDripsUserMapping.load(
+      lastDripsSetUserMappingId
+    ) as LastSetDripsUserMapping;
+    assert.stringEquals(lastDripsSetUserMapping.dripsSetEventId, dripsSetEventId);
+    assert.stringEquals(
+      lastDripsSetUserMapping.userId,
+      incomingDripsSetEvent.params.userId.toString()
+    );
+    assert.bigIntEquals(lastDripsSetUserMapping.assetId, incomingDripsSetEvent.params.assetId);
+  });
+
+  test('should update entities as expected when mapping', () => {
+    // Arrange
+    const userId = BigInt.fromI32(1);
+    const user = new User(userId.toString());
+    user.save();
+
+    const incomingDripsSetEvent = createDripsSetEvent(
+      userId,
+      BigInt.fromI32(2),
+      Bytes.fromUTF8('receiversHash'),
+      Bytes.fromUTF8('dripsHistoryHash'),
+      BigInt.fromI32(3),
+      BigInt.fromI32(4),
+      BigInt.fromI32(5),
+      Bytes.fromUTF8('transactionHash')
+    );
+
+    const dripsEntryId = '1';
+    const dripsEntry = new DripsEntry(dripsEntryId);
+    dripsEntry.save();
+
+    const userAssetConfigId = `${incomingDripsSetEvent.params.userId.toString()}-${incomingDripsSetEvent.params.assetId.toString()}`;
+    let userAssetConfig = new UserAssetConfig(userAssetConfigId);
+    userAssetConfig.dripsEntryIds = [dripsEntryId];
+    userAssetConfig.save();
+
+    // Act
+    handleDripsSet(incomingDripsSetEvent);
+
+    // Assert
+    assert.assertNull(DripsEntry.load(dripsEntryId));
+
+    userAssetConfig = UserAssetConfig.load(userAssetConfigId) as UserAssetConfig;
+    assert.bigIntEquals(userAssetConfig.balance, incomingDripsSetEvent.params.balance);
+    assert.bytesEquals(userAssetConfig.assetConfigHash, incomingDripsSetEvent.params.receiversHash);
+    assert.bigIntEquals(
+      userAssetConfig.lastUpdatedBlockTimestamp,
+      incomingDripsSetEvent.block.timestamp
+    );
+    assert.arrayEquals(
+      userAssetConfig.dripsEntryIds.map<ethereum.Value>((s) => ethereum.Value.fromString(s)),
+      []
+    );
+
+    const dripsSetEventId = `${incomingDripsSetEvent.transaction.hash.toHexString()}-${incomingDripsSetEvent.logIndex.toString()}`;
+    const dripsSetEventEntity = DripsSetEvent.load(dripsSetEventId) as DripsSetEvent;
+    assert.stringEquals(dripsSetEventEntity.userId, incomingDripsSetEvent.params.userId.toString());
+    assert.bigIntEquals(dripsSetEventEntity.assetId, incomingDripsSetEvent.params.assetId);
+    assert.bytesEquals(
+      dripsSetEventEntity.receiversHash,
+      incomingDripsSetEvent.params.receiversHash
+    );
+    assert.bytesEquals(
+      dripsSetEventEntity.dripsHistoryHash,
+      incomingDripsSetEvent.params.dripsHistoryHash
+    );
+    assert.bigIntEquals(dripsSetEventEntity.balance, incomingDripsSetEvent.params.balance);
+    assert.bigIntEquals(dripsSetEventEntity.maxEnd, incomingDripsSetEvent.params.maxEnd);
+    assert.bigIntEquals(dripsSetEventEntity.blockTimestamp, incomingDripsSetEvent.block.timestamp);
+
+    const lastDripsSetUserMappingId = incomingDripsSetEvent.params.receiversHash.toHexString();
+    const lastDripsSetUserMapping = LastSetDripsUserMapping.load(
+      lastDripsSetUserMappingId
+    ) as LastSetDripsUserMapping;
+    assert.stringEquals(lastDripsSetUserMapping.dripsSetEventId, dripsSetEventId);
+    assert.stringEquals(
+      lastDripsSetUserMapping.userId,
+      incomingDripsSetEvent.params.userId.toString()
+    );
+    assert.bigIntEquals(lastDripsSetUserMapping.assetId, incomingDripsSetEvent.params.assetId);
   });
 });
--- tests/helpers/eventCreators.ts
+++ tests/helpers/eventCreators.ts
@@ -1,21 +1,50 @@
 /* eslint-disable @typescript-eslint/ban-types */
-import { BigInt, ethereum } from '@graphprotocol/graph-ts';
+import { BigInt, Bytes, ethereum } from '@graphprotocol/graph-ts';
 import { newMockEvent } from 'matchstick-as/assembly/index';
 import { DripsSet } from '../../generated/DripsHub/DripsHub';
 
-export function createDripsSetEvent(id: BigInt, assetId: BigInt): DripsSet {
+export function createDripsSetEvent(
+  userId: BigInt,
+  assetId: BigInt,
+  receiversHash: Bytes,
+  dripsHistoryHash: Bytes,
+  balance: BigInt,
+  maxEnd: BigInt,
+  blockTimestamp: BigInt,
+  transactionHash: Bytes
+): DripsSet {
   const dripsSetEvent = changetype<DripsSet>(newMockEvent());
 
   dripsSetEvent.parameters = [];
 
-  const idParam = new ethereum.EventParam('id', ethereum.Value.fromUnsignedBigInt(id));
+  const userIdParam = new ethereum.EventParam('id', ethereum.Value.fromUnsignedBigInt(userId));
   const assetIdParam = new ethereum.EventParam(
     'assetId',
     ethereum.Value.fromUnsignedBigInt(assetId)
   );
+  const receiversHashParam = new ethereum.EventParam(
+    'receiversHash',
+    ethereum.Value.fromBytes(receiversHash)
+  );
+  const dripsHistoryHashParam = new ethereum.EventParam(
+    'dripsHistoryHash',
+    ethereum.Value.fromBytes(dripsHistoryHash)
+  );
+  const balanceParam = new ethereum.EventParam(
+    'balance',
+    ethereum.Value.fromUnsignedBigInt(balance)
+  );
+  const maxEndParam = new ethereum.EventParam('maxEnd', ethereum.Value.fromUnsignedBigInt(maxEnd));
 
-  dripsSetEvent.parameters.push(idParam);
+  dripsSetEvent.parameters.push(userIdParam);
   dripsSetEvent.parameters.push(assetIdParam);
+  dripsSetEvent.parameters.push(receiversHashParam);
+  dripsSetEvent.parameters.push(dripsHistoryHashParam);
+  dripsSetEvent.parameters.push(balanceParam);
+  dripsSetEvent.parameters.push(maxEndParam);
+
+  dripsSetEvent.block.timestamp = blockTimestamp;
+  dripsSetEvent.transaction.hash = transactionHash;
 
   return dripsSetEvent;
 }
